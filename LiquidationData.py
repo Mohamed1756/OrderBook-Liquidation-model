054e036ccd1f3ed9ae3346cd394a2d81eb25d383
Mohamed A.M <Mohamed.m365@outlook.com>
2023-07-15 10:59:56 +0100

    amended liq data code

--- LiquidationData.py
+++ LiquidationData.py
@@ -2,15 +2,15 @@ import asyncio
 import json
 import os
 from websockets import connect
+from datetime import datetime, timezone
 
 websocket_uri = "wss://fstream.binance.com/ws/!forceOrder@arr"
 filename = "binance.csv"
 
 if not os.path.isfile(filename):
     with open(filename, "w") as f:
-        f.write(",".join(["Symbol", "Side", "Order Type", "Time in Force", "Original Quantity",
-                          "Price", "Average Price", "Order Status", "Order Last Filled Quantity",
-                          "Order Filled Accumulated Quantity", "Order Trade Time"]) + "\n")
+        f.write(",".join(["Symbol", "Side", "Order Type", "Original Quantity",
+                          "Liq Price", "Order Status", "TimeStamp", "Value"]) + "\n")
 
 
 async def binance_liquidations(uri, filename):
@@ -20,9 +20,24 @@ async def binance_liquidations(uri, filename):
                 msg = await websocket.recv()
                 print(msg)
                 msg = json.loads(msg)["o"]
-                msg = [str(x) for x in list(msg.values())]
+                symbol = msg["s"]
+                side = msg["S"]
+                order_type = msg["o"]
+                quantity = float(msg["q"])
+
+                average_price = float(msg["ap"])
+                order_status = msg["X"]
+                timestamp = int(msg["T"])
+                value = quantity * average_price
+
+                # Convert timestamp to UTC datetime
+                trade_time = datetime.fromtimestamp(timestamp / 1000.0, tz=timezone.utc).strftime('%H:%M:%S')
+
+                data = [symbol, side, order_type, str(quantity),str(average_price), order_status,
+                        trade_time, str(value)]
+
                 with open(filename, "a") as f:
-                    f.write(",".join(msg) + "\n")
+                    f.write(",".join(data) + "\n")
         except Exception as e:
             print(e)
             continue
